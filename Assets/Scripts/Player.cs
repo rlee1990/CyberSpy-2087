b8d6e0772059655570a29819c22f948a28dd47fa
rlee1990 <rickeylee1129@gmail.com>
2023-03-20 22:26:55 -0400

    Added Effects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,6 +18,8 @@ public class Player : MonoBehaviour
     public GameObject bullet;
     public Transform firePosition;
 
+    public GameObject muzzleFlash, bulletHole;
+
 
     // Start is called before the first frame update
     void Start()
@@ -51,6 +53,7 @@ public class Player : MonoBehaviour
             {
                 firePosition.LookAt(myCameraHead.position + (myCameraHead.forward * 50f));
             }
+            Instantiate(muzzleFlash, firePosition.position, firePosition.rotation, firePosition);
             Instantiate(bullet, firePosition.position, firePosition.rotation);
         }
     }
